77259325b476adf420e2fbe960ac7280f8e1b2dc
unknown <asasbo21@gmail.com>
2023-12-12 17:03:48 +0900

    2commit

--- src/SpringLegacyProject_ex/SpringLegacyProject_ex.java
+++ src/SpringLegacyProject_ex/SpringLegacyProject_ex.java
@@ -5,6 +5,7 @@ public class SpringLegacyProject_ex {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("hello");
 	}
 
 }
